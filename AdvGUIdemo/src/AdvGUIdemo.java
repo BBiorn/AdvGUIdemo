cfe53369fbc0bf1b7d47145694bebc2b3667a51b
ginhi_000 <ginhi_000@DESKTOP-CF29UVU>
2018-03-28 19:53:07 -0500

    Created FoodItem class and added list to show cart items

--- AdvGUIdemo/src/AdvGUIdemo.java
+++ AdvGUIdemo/src/AdvGUIdemo.java
@@ -1,10 +1,13 @@
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Vector;
+
 import javax.swing.*;
 
 public class AdvGUIdemo 
 {
+   static Vector<FoodItem> foodList = new Vector<FoodItem>();//used to store food items in the cart
    public static void main(String[] Args)
    {
 	   JFrame frame = new JFrame("Food Supply");//UI Frame
@@ -14,6 +17,11 @@ public class AdvGUIdemo
 	   
 	   JPanel mainPanel = new JPanel();//Main JPanel to add onto
 	   mainPanel.setLayout(new BorderLayout());
+	   
+	   JList foodListJL = new JList(foodList);//This is going to show the food items in the cart
+	   JScrollPane foodScrollPane = new JScrollPane(foodListJL);
+	   mainPanel.add(foodScrollPane, BorderLayout.EAST);
+	   
 	   
 	   frame.add(mainPanel);
        frame.setVisible(true);	   
