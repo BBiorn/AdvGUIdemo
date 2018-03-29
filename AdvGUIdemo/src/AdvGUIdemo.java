30b8fcb94554484e5b0a1bfae0870ebaaa458eef
ginhi_000 <ginhi_000@DESKTOP-CF29UVU>
2018-03-28 19:33:33 -0500

    Created the UI window

--- AdvGUIdemo/src/AdvGUIdemo.java
+++ AdvGUIdemo/src/AdvGUIdemo.java
@@ -7,6 +7,15 @@ public class AdvGUIdemo
 {
    public static void main(String[] Args)
    {
+	   JFrame frame = new JFrame("Food Supply");//UI Frame
+	   frame.setSize(800,600);
+	   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	   frame.setResizable(false);
 	   
+	   JPanel mainPanel = new JPanel();//Main JPanel to add onto
+	   mainPanel.setLayout(new BorderLayout());
+	   
+	   frame.add(mainPanel);
+       frame.setVisible(true);	   
    }
 }
